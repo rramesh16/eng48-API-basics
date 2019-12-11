6fedbace743ec51cb3a638fa977e74553159d20a
Rahavi Ramesh <RRamesh@spartaglobal.com>
2019-12-11 16:07:09 +0000

    Added codes to build url, turn this into dictionary, geeting out data

--- get_requests.py
+++ get_requests.py
@@ -1,9 +1,24 @@
 import requests
 
 # build url
-path_url = 'http://api.postcodes.io/postocdes/'
+path_url = 'http://api.postcodes.io/postcodes/'
 arguments = 'e146gt'
 
 
 post_codes = requests.get(path_url + arguments)
 print(post_codes)
+print(type(post_codes))
+
+# Turn this into a dictionary
+dict_response = post_codes.json()
+print(dict_response)
+
+# Getting out data
+print(dict_response['status'])
+
+print(dict_response['result'].keys())
+print(dict_response['result']['admin_district'])
+
+for key in dict_response ['result']:
+    print('key:', key)
+    print (key, '-->', dict_response['result'][key])
